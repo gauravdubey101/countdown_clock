229e8566e4708673c9c9057b719764c4be8df49c
Gaurav Dubey <gauravd638@gmail.com>
2021-01-25 23:57:43 +0530

    js is added

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const newYear = '1st jan 2020';
+const dayE = document.getElementById('days');
+const hoursE = document.getElementById('hours');
+const minsE = document.getElementById('mins');
+const secsE = document.getElementById('seconds');
+
+
+
+
+
+
+const newYears = '1 jan 2022';
 
 
 function countdown(){
+    const newYearDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate-currentDate)/1000;
+    const days = Math.floor(totalSeconds/3600/24);
+    const  hours = Math.floor(totalSeconds/3600)%24;
+    const mins = Math.floor(totalSeconds/60)%60;
+    const seconds = Math.floor(totalSeconds)%60;
+
+    /* console.log(totalSeconds);
+ */
+    /* console.log(days,hours,minutes,seconds); */
+
+    dayE.innerHTML = days;
+    hoursE.innerHTML = hours;
+    minsE.innerHTML = mins;
+    secsE.innerHTML = seconds;
     
+
 }
+countdown();
+
+setInterval(countdown,1000);
+
