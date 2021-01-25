fb3a18178d203466a2cfe55efe0c0b79bcb37701
Gaurav Dubey <gauravd638@gmail.com>
2021-01-26 00:38:10 +0530

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const dayE = document.getElementById('days');
+const daysE = document.getElementById('days');
 const hoursE = document.getElementById('hours');
 const minsE = document.getElementById('mins');
 const secsE = document.getElementById('seconds');
@@ -12,10 +12,10 @@ const newYears = '1 jan 2022';
 
 
 function countdown(){
-    const newYearDate = new Date(newYears);
+    const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearDate-currentDate)/1000;
+    const totalSeconds = (newYearsDate-currentDate)/1000;
     const days = Math.floor(totalSeconds/3600/24);
     const  hours = Math.floor(totalSeconds/3600)%24;
     const mins = Math.floor(totalSeconds/60)%60;
@@ -23,16 +23,23 @@ function countdown(){
 
     /* console.log(totalSeconds);
  */
-    /* console.log(days,hours,minutes,seconds); */
+    console.log(days,hours,mins,seconds);
 
-    dayE.innerHTML = days;
-    hoursE.innerHTML = hours;
-    minsE.innerHTML = mins;
-    secsE.innerHTML = seconds;
+    daysE.innerHTML = days;
+     hoursE.innerHTML = formateTime(hours);
+   minsE.innerHTML = formateTime(mins);
+   secsE.innerHTML = formateTime(seconds); 
     
 
 }
+
+function formateTime(time){
+    return time <10?`0${time}`:time;
+}
+
+
 countdown();
+
 
 setInterval(countdown,1000);
 
